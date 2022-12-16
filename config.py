794612e5665d42d94a0dc8e3319b77f86c006e26
chunhou20c <youmustbekiddingmeshit@gmail.com>
2022-12-17 02:34:21 +0800

    remove unused config option

--- config.py
+++ config.py
@@ -1,7 +1,10 @@
-# this configuration file define the parameters used in the training
-DATA_DIRECTORY = "/home/ChunHou/Project/ANN_charge-transfer-coupling/data/DBT1"
-FEATURE = "CM_matrix.npy"
-RESULT = "results.npy"
+"""
+This file contains all the configuration for the training of model
+"""
+
+DATA_DIRECTORY = "./data"
+FEATURE = "CM-inter-duplicate/feature.npy"
+RESULT = "CM-inter-duplicate/label.npy"
 LEARNING_RATE = 0.006
 METRICS = 'MeanAbsolutePercentageError'
 LOSS = 'Huber'
@@ -14,7 +17,7 @@ SAVE_PATH = 'result'
 DECAY = True
 DECAY_RATE = 0.02
 
-EARLY_STOP = False
+EARLY_STOP = True
 #configuration related to early stop
 #option = "loss" or "metrics"
 MONITOR = "loss" 
@@ -22,10 +25,6 @@ PATIENCE = 3
 
 CHECKPOINT = True
 CHECKPOINT_PATH = "checkpoint/cp.ckpt"
-
-if SCALE == True:
-    global FEATURE_RANGE
-    FEATURE_RANGE = (0,1)
 
 import tensorflow as tf
 OPTIMIZER = tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE)
